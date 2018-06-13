aafe1574b03f36acb435028d256892096f12dae7
CortezZz <mailto:cortezzz1987@gmail.com>
2018-06-13 12:04:51 +0300

    Generated JavaDoc for Calculate

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,12 +2,12 @@ package ru.job4j.calculate;
 
 /**
 *Класс Calculate для выполнения задачи
-*@autor Maksim Yunusov (mailto:CortezZz1987@gmail.com)
+*@author Maksim Yunusov (mailto:CortezZz1987@gmail.com)
 */
 public class Calculate {
 	/** method main
 	*Выводит текст на экран
-	*@param args
+	*@param args - args
 	*/
 	public static void main (String[] args) {
 		System.out.println ("Hello world!");
