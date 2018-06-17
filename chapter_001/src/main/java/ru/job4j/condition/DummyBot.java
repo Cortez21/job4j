9738bc2afa1737fdb0aff8690b1f3d9b94c8e68e
CortezZz <mailto:cortezzz1987@gmail.com>
2018-06-17 22:47:31 +0300

    Added DummyBot's files and tests

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+/**
+ * @author Maksim Yunusov (mailto:cortezzz1987@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class DummyBot {
+
+    /**
+     * Основной метод бота который управляет диалогом с клиентом.
+     * @param question Входные данные от клиента.
+     * @return Ответ бота.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+
+}
