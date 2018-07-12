2a36387731b53fba096a7ebd187b8e47aeea6c3b
Cortez21 <mailto:cortezzz1987@gmail.com>
2018-07-12 20:35:01 +0300

    Fixed ArrayChar class

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -12,6 +12,9 @@ public class MatrixCheck {
      */
     public boolean mono(boolean[][] data) {
         boolean result = true;
+        if (data[0][0] != data[0][data.length - 1]) {
+            result = false;
+        }
         for (int i = 0; i < data.length - 1; i++) {
             if (data[i][i] != data[i + 1][i + 1]) {
                 result = false;
@@ -19,6 +22,7 @@ public class MatrixCheck {
             if (data[i][data.length - 1 - i] != data[i + 1][data.length - 2 - i]) {
                 result = false;
             }
+
         }
         return result;
     }
