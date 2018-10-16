855a6a3b9b9e49e7576a5b59cb6208109591e5e4
Cortez21 <CortezZz1987@gmail.com>
2018-10-16 10:45:55 +0300

    Fix for StartUI.showMenu() method

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -128,7 +128,9 @@ public class StartUI {
      * Print menu of program interface
      */
     private void showMenu() {
-        System.out.println("0. Add new Item" + ls() + "1. Show all items" + ls() + "2. Edit item" + ls() + "3. Delete item" + ls() + "4. Find item by Id" + ls() + "5. Find items by name" + ls() + "6. Exit Program");
+        StringBuilder builder = new StringBuilder();
+        //System.out.println("0. Add new Item" + ls() + "1. Show all items" + ls() + "2. Edit item" + ls() + "3. Delete item" + ls() + "4. Find item by Id" + ls() + "5. Find items by name" + ls() + "6. Exit Program");
+        System.out.println(builder.append("0. Add new Item").append(ls()).append("1. Show all items").append(ls()).append("2. Edit item").append(ls()).append("3. Delete item").append(ls()).append("4. Find item by Id").append(ls()).append("5. Find items by name").append(ls()).append("6. Exit Program").append(ls()));
     }
 
     public static void main(String[] args) {
