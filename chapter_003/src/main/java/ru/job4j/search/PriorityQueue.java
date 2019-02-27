9cb54bc07dac2ffba735184beeae0a7c52931fc0
Cortez21 <CortezZz1987@gmail.com>
2019-02-27 22:45:32 +0200

    Next fix for PriorityQueue

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -10,42 +10,20 @@ public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
     /**
-     * Метод должен вставлять в нужную позицию элемент.
-     * Позиция определять по полю приоритет.
-     * Для вставки использовать add(int index, E value)
-     * @param task задача
-     */
-    public void put(Task task) {
-        tasks.add(getPosition(task), task);
-    }
-
-    /**
      * Calculate position of current task for the LinkedList
      * @param task current task
      * @return number of position
      */
-
-    private int getPosition(Task task) {
-        int result = -1;
-        if (tasks.isEmpty()) {
-            result = 0;
-        } else if (tasks.size() == 1 && tasks.get(0).getPriority() > task.getPriority()) {
-            result = 0;
-        } else if (tasks.size() == 1 && tasks.get(0).getPriority() > task.getPriority()) {
-            result = 1;
-        } else {
-            for (Task curTask : tasks) {
-                if (task.getPriority() < curTask.getPriority()) {
-                    result = tasks.indexOf(curTask);
-                    break;
-                } else if (curTask.equals(tasks.getLast()) && curTask.getPriority() < task.getPriority()) {
-                    result = tasks.size();
-                }
+    public void put(Task task) {
+        int index = tasks.size();
+        for (int i = 0; i < tasks.size(); i++) {
+            if (task.getPriority() < tasks.get(i).getPriority()) {
+                index = i;
+                break;
             }
         }
-        return result;
+        tasks.add(index, task);
     }
-
     public Task take() {
         return this.tasks.poll();
     }
