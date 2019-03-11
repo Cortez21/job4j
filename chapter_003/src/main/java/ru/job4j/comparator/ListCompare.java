ad8b8a60e20e09c7468d6a6052e23485d3bbd96d
Cortez21 <CortezZz1987@gmail.com>
2019-03-11 14:37:44 +0200

    Some fix for ListCompare

--- chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -1,8 +1,7 @@
 package ru.job4j.comparator;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
+
 
 
 /**
@@ -20,36 +19,17 @@ public class ListCompare implements Comparator<String> {
      */
     @Override
     public int compare(String left, String right) {
-        List<Character> leftList = new ArrayList<>();
-        List<Character> rightList = new ArrayList<>();
         int result = 0;
-        for (int i = 0; i < Integer.max(left.length(), right.length()); i++) {
-            if (i >= left.length()) {
-                leftList.add(null);
-                rightList.add(right.charAt(i));
-            } else if (i >= right.length()) {
-                leftList.add(left.charAt(i));
-                rightList.add(null);
-            } else {
-                rightList.add(right.charAt(i));
-                leftList.add(left.charAt(i));
-            }
+        for (int i = 0; i < Integer.min(left.length(), right.length()); i++) {
+            result += Character.compare(left.charAt(i), right.charAt(i));
         }
 
-        for (int i = 0; i < Integer.max(leftList.size(), rightList.size()); i++) {
-            if (rightList.get(i) == null) {
-                result = 1;
-                break;
-            } else if (leftList.get(i) == null) {
-                result = -1;
-            } else if (leftList.get(i) > rightList.get(i)) {
-                result = 1;
-                break;
-            } else if (leftList.get(i) < rightList.get(i)) {
-                result = -1;
-                break;
-            }
+        if (result == 0 && left.length() > right.length()) {
+            result = 1;
+        } else if (result == 0 && left.length() < right.length()) {
+            result = -1;
         }
+
         return result;
     }
 }
