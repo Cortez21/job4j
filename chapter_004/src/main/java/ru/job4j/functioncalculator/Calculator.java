5f7e0bffe49bf4b9c4dc6cfff814e3bf98426a3f
Cortez21 <CortezZz1987@gmail.com>
2019-03-26 20:37:04 +0200

    Added class and test for Calculator - the program for calculating values of some mathimatical functions

--- chapter_004/src/main/java/ru/job4j/functioncalculator/Calculator.java
+++ chapter_004/src/main/java/ru/job4j/functioncalculator/Calculator.java
@@ -1,4 +1,15 @@
 package ru.job4j.functioncalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 public class Calculator {
+    public List<Double> calculate(int start, int finish, Function<Integer, Double> function) {
+        List<Double> values = new ArrayList<>();
+        for (int index = start; index < finish; index++) {
+            values.add(Double.valueOf(function.apply(index)));
+        }
+        return values;
+    }
 }
--- chapter_004/src/test/java/ru/job4j/functioncalculator/CalculatorTest.java
+++ chapter_004/src/test/java/ru/job4j/functioncalculator/CalculatorTest.java
@@ -1,4 +1,38 @@
 package ru.job4j.functioncalculator;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.Math.*;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
 public class CalculatorTest {
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        Calculator calculator = new Calculator();
+        List<Double> result = calculator.calculate(5, 8, x -> Double.valueOf(2 * x + 1));
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenQuadraticFunctionThenQuadraticResults() {
+        Calculator calculator = new Calculator();
+        List<Double> result = calculator.calculate(5, 8, x -> pow(2 * x, 2) + (2 * 3) + 4);
+        List<Double> expected = Arrays.asList(110D, 154D, 206D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenLogarithmicFunctionThenLogarithmicResults() {
+        Calculator calculator = new Calculator();
+        List<Double> result = calculator.calculate(5, 8, x -> Double.valueOf(round(log(3D) * x)));
+        List<Double> expected = Arrays.asList(5D, 7D, 8D);
+        assertThat(result, is(expected));
+    }
+
 }
