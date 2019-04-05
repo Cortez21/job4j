6d4c570bf9b4cc849a55a7a18c1f175af95c2384
Cortez21 <CortezZz1987@gmail.com>
2019-04-05 14:16:49 +0300

    Added new program for convert matrix of values to list

--- chapter_004/src/main/java/ru/job4j/matrixtolist/MatrixToList.java
+++ chapter_004/src/main/java/ru/job4j/matrixtolist/MatrixToList.java
@@ -1,4 +1,14 @@
 package ru.job4j.matrixtolist;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MatrixToList {
+    public List<Integer> convert(int[][] matrix) {
+
+        return Arrays.stream(matrix)
+                .flatMap(e -> Arrays.stream(e).boxed())
+                .collect(Collectors.toList());
+    }
 }
--- chapter_004/src/test/java/ru/job4j/matrixtolist/MatrixToListTest.java
+++ chapter_004/src/test/java/ru/job4j/matrixtolist/MatrixToListTest.java
@@ -1,4 +1,15 @@
 package ru.job4j.matrixtolist;
 
+import com.sun.tools.javac.util.List;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class MatrixToListTest {
+    @Test
+    public void whenMatrixConvertToList() {
+        MatrixToList matrixToList = new MatrixToList();
+        assertThat(matrixToList.convert(new int[][] {{1, 2}, {3, 4}, {5, 6}}), is(List.of(1, 2, 3, 4, 5, 6)));
+    }
 }
