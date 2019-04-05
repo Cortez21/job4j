af8cca596af53f004d24866344f33050c3f80510
Cortez21 <CortezZz1987@gmail.com>
2019-04-05 14:15:02 +0300

    Some fix for Profiles class

--- chapter_004/src/main/java/ru/job4j/adresses/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/adresses/Profiles.java
@@ -7,7 +7,10 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).distinct().sorted(Comparator.comparing(Address::getCity)).collect(Collectors.toList());
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .distinct().sorted(Comparator.comparing(Address::getCity))
+                .collect(Collectors.toList());
     }
 
 
